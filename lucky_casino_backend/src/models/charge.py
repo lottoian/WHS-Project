32b776d4b4db7c0a7681b15aff7230ff35079e90
dlwodnr <dlwodnr0034@naver.com>
2025-07-08 05:32:21 -0700

    오류제거

--- lucky_casino_backend/src/models/charge.py
+++ lucky_casino_backend/src/models/charge.py
@@ -1,4 +1,4 @@
-from ..models import db
+from models import db
 from sqlalchemy import func
 
 class Charge(db.Model):
--- lucky_casino_backend/src/models/withdraw.py
+++ lucky_casino_backend/src/models/withdraw.py
@@ -1,4 +1,4 @@
-from ..models import db
+from models import db
 from sqlalchemy import func
 
 class Withdraw(db.Model):
